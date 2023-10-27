2319794f27a43bbacc24b5ebacf22606496b81eb
osamaalsad <o.alsad1998@gmail.com>
2023-10-27 18:22:59 +0300

    Add jwt authentications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+    include JsonWebToken
+
+    before_action :authenticate_request
+
+    private
+        def authenticate_request
+            header = request.headers["Authorization"]
+            if header.nil? || header.blank?
+                render json: { error: 'Unauthorized' }, status: :unauthorized
+            else
+                header = header.split(" ").last
+                decoded = jwt_decode(header)
+                @current_user = User.find(decoded[:user_id])
+            end
+        end
 end
