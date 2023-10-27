b90b81faf77bb7e5dce85a5f607f37dfeebb6e8c
osamaalsad <o.alsad1998@gmail.com>
2023-10-27 18:59:46 +0300

    fixup! Add job post model

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,3 +1,13 @@
 class JobPost < ApplicationRecord
   belongs_to :user
+  before_validation :set_default_date, on: :create
+
+  # Other model code
+
+  private
+
+  def set_default_date
+    self.date_posted ||= Date.current
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_27_150704) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_27_153440) do
+  create_table "job_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date_posted"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_job_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -19,4 +29,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_27_150704) do
     t.string "role"
   end
 
+  add_foreign_key "job_posts", "users"
 end
