ac9503f1b99c4de976685153cb0be1ac8888f27d
osamaalsad <o.alsad1998@gmail.com>
2023-10-27 18:39:44 +0300

    Add job post model

---
+++ db/migrate/20231027153440_create_job_posts.rb
@@ -0,0 +1,12 @@
+class CreateJobPosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :job_posts do |t|
+      t.string :title
+      t.text :description
+      t.date :date_posted
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
