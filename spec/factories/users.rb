578f4e9030571e0d6c1114b78ae6b07568c5a677
osamaalsad <o.alsad1998@gmail.com>
2023-10-30 21:21:28 +0300

    Add rspec test files for all models

---
+++ spec/factories/users.rb
@@ -0,0 +1,20 @@
+
+FactoryBot.define do
+    factory :user do
+        email { Faker::Internet.email(domain: 'jobboard.com') }
+        password { "password" } # Set a default password for testing
+    end
+
+    factory :admin_user, class: User do
+      email { Faker::Internet.email(domain: 'jobboard_admin.com') }
+      password { "password" }
+      role { "admin" }
+    end
+  
+    factory :job_seeker_user, class: User do
+      email { Faker::Internet.email(domain: 'jobboard_jobseeker.com') }
+      password { "password" }
+      role { "job_seeker" }
+    end
+  end
+  
